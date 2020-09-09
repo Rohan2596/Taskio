d02ef0f8f61a36dba837a694e9b96947112cfb29
Rohan2596 <rohankadam662@gmail.com>
2020-09-09 13:28:50 +0530

    ADDED:Create Activity and FAB button Click.

--- app/src/main/java/com/spatalabz/taskio/MainActivity.java
+++ app/src/main/java/com/spatalabz/taskio/MainActivity.java
@@ -2,13 +2,29 @@ package com.spatalabz.taskio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent toTask=new Intent(MainActivity.this,CreateActivity.class);
+        startActivity(toTask);
+        floatingActionButton=findViewById(R.id.createTask);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toTask=new Intent(MainActivity.this,CreateActivity.class);
+                startActivity(toTask);
+            }
+        });
     }
 }
